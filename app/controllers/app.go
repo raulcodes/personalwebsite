bc6340de52e6a723ad87d20cf09d3481be11d6b3
Raul Camacho <rcamacho96@gmail.com>
2016-05-29 23:20:30 -0500

    personal site, first commit

---
+++ app/controllers/app.go
@@ -0,0 +1,19 @@
+package controllers
+
+import "github.com/revel/revel"
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	return c.Render()
+}
+
+func (c App) Resume() revel.Result {
+	return c.Render()
+}
+
+func (c App) Writings() revel.Result {
+	return c.Render()
+}
